10ae560d22accea0472e997a742ab56b5c6f180f
8wgf3b <karnabalaj@gmail.com>
2024-11-04 15:28:22 +0000

    only info log

--- spam/src/main.rs
+++ spam/src/main.rs
@@ -51,7 +51,7 @@ async fn function_handler(db: &Daybreak, event: LambdaEvent<Request>) -> Result<
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         // disable printing the name of the module in every log line.
         .with_target(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
